24b2551d9bd2eb777480dab458f3744faba2bb3a
Muhammad Andreas Athallah Saifa Anam <andreasnation970@gmail.com>
2024-11-05 21:12:12 +0700

    V46 - 50

---
+++ ProjectPHPOop4/restoran/LATIHAN/function.php
@@ -0,0 +1,11 @@
+<?php
+    $host = "127.0.0.1";
+    $user = "root";
+    $password =  "";
+    $database = "dbretoran";
+
+    $koneksi = mysqli_connect($host, $user , $password, $database);
+
+
+
+?>
